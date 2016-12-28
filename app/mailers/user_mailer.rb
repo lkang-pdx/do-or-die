c95ccc76032bd4c6a1b74aad09e3582d54319ad0
Luna Kang <luna.kang.ca@gmail.com>
2016-12-28 07:50:02 -0800

    Removed redundant user_mailer.rb

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "luna.kang.ca@gmail.com"
-
-  def new_user(user)
-    @user = user
-    mail(to: user.email, subject: "Thank you for joining Do or Die!")
-  end
-end
