7b43f9044e5ac74ccb9c0d225a8046db0b97578b
Luna Kang <luna.kang.ca@gmail.com>
2017-01-01 18:30:36 -0800

    Partial edits

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
   def create
+    @user = current_user
     @item = current_user.items.build(item_params)
 
     if @item.save
       flash[:notice] = "Item was saved."
-      redirect_to @item
+      redirect_to @user
     else
       flash.now[:alert] = "There was an error saving the item. Please try again."
       render :new
