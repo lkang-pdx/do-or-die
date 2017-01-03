c6cb763bccec887b3b93c706cee9dbc80cabe878
Luna Kang <luna.kang.ca@gmail.com>
2017-01-02 16:07:13 -0800

    CP 7: Mark To-Do Items as Complete (#4)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item.name}\" was deleted successfully."
+    else
+      flash.now[:alert] = "There was an error deleting the item."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name)
